d639eea0058c2eca9a71e0889b97e6bb14043ad9
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-06-17 01:13:00 -0700

    fixing betty code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-	/*
-	 * main function is denoted by int main(void)
- 	 * return always (0)
-	 *
-	 */
+/*
+ * main function is denoted by int main(void)
+ * return always (0)
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
