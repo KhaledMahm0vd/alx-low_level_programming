33e181f2c0dcd737dc864fd78fe7038074f8cc30
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-06-17 01:00:30 -0700

    updating docs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*
  *using main as function to pass the code inside it.
  */
-int main(void)
+int main(void)// main function
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
