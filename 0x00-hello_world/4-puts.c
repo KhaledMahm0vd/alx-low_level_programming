4b6a30add68de3958acf91e4c41f412100d918b0
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-06-17 01:18:53 -0700

    fixing description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-/*
+ /**
  * main - Entry point
- *using main as function to pass the code inside it.
- *
- * main function is denoted by int main(void)
- * return always (0)
+ * 
+ * Description: 'using puts to print a statement'
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-/*
- * main function is denoted by int main(void)
- * return always (0)
- */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
