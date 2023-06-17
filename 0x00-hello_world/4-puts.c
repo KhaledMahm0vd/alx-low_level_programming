0e5bbae9434ebc240dbe67cef874da851bb7ecb7
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-06-17 01:08:32 -0700

    another update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
  *using main as function to pass the code inside it.
  *
  * main function is denoted by int main(void)
+ * return always (0)
  */
-int main(void)			/* main function*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
