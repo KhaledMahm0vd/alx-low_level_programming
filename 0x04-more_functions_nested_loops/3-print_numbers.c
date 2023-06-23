623c183faea396013c8abd9fe707ac78b910919f
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-06-23 11:33:59 -0700

    3-1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-/*
- * print_numbers - print numbers from 0 to 9
- * followed by new line.
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-
-void print_numbers(void);
+void print_numbers(void)
 {
 	int i;
 
