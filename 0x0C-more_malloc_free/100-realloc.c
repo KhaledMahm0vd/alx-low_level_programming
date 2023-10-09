180a0ea5855c4cadfd902dff8f0ea50cef6dbd69
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-10-09 21:36:08 +0300

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,51 +9,45 @@
  *
  * Return: pointer to the newly allocated memory block
  */
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
-  char *ptr1;
-  char *old_ptr;
-  unsigned int i;
-
-  // Check if the new size is the same as the old size
-  if (new_size == old_size) {
-    return ptr;
-  }
-
-  // Check if the new size is 0 and the pointer is not NULL
-  if (new_size == 0 && ptr != NULL) {
-    free(ptr);
-    return NULL;
-  }
-
-  // Check if the pointer is NULL
-  if (ptr == NULL) {
-    return malloc(new_size);
-  }
-
-  // Allocate new memory for the pointer
-  ptr1 = malloc(new_size);
-
-  // Check if the memory allocation failed
-  if (ptr1 == NULL) {
-    return NULL;
-  }
-
-  // Copy the old data to the new memory
-  old_ptr = ptr;
-  if (new_size < old_size) {
-    for (i = 0; i < new_size; i++) {
-      ptr1[i] = old_ptr[i];
-    }
-  } else {
-    for (i = 0; i < old_size; i++) {
-      ptr1[i] = old_ptr[i];
-    }
-  }
-
-  // Free the old memory
-  free(ptr);
-
-  // Return the pointer to the new memory
-  return ptr1;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+	{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+	if (new_size == old_size)
+		{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+		{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	ptr1 = malloc(new_size);
+	if (ptr1 == NULL)
+		{
+		return (NULL);
+		}
+	old_ptr = ptr;
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			{
+			ptr1[i] = old_ptr[i];
+			}
+	}
+	else
+	{
+		for (i = 0; i < old_size; i++)
+			{
+			ptr1[i] = old_ptr[i];
+			}
+	}
+	free(ptr);
+	return (ptr1);
 }
 
