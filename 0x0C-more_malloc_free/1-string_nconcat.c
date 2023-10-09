eeffe020155026f5997f721650482e842a2261b1
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-10-09 21:02:05 +0300

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,46 +9,42 @@
  *
  * Return: pointer to the resulting string
  */
- char *string_nconcat(char *s1, char *s2, unsigned int n) {
-  char *s;
-  unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-  // Get the lengths of the two strings
-  while (s1 && s1[len1])
-    len1++;
-  while (s2 && s2[len2])
-    len2++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-  // Allocate enough memory for the new string
-  if (n < len2)
-    s = malloc(sizeof(char) * (len1 + n + 1));
-  else
-    s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-  // Check if the memory allocation failed
-  if (!s)
-    return NULL;
+	if (!s)
+		return (NULL);
 
-  // Copy the first string to the new string
-  while (i < len1) {
-    s[i] = s1[i];
-    i++;
-  }
+	while (i < len1)
+		{
+		s[i] = s1[i];
+		i++;
+		}
 
-  // Copy the first n characters of the second string to the new string
-  while (n < len2 && i < (len1 + n)) {
-    s[i++] = s2[j++];
-  }
+	while (n < len2 && i < (len1 + n))
+		{
+		s[i++] = s2[j++];
+		}
 
-  // Copy the remaining characters of the second string to the new string
-  while (n >= len2 && i < (len1 + len2)) {
-    s[i++] = s2[j++];
-  }
+	while (n >= len2 && i < (len1 + len2))
+		{
+		s[i++] = s2[j++];
+		}
 
-  // Terminate the new string
-  s[i] = '\0';
+	s[i] = '\0';
 
-  // Return the new string
-  return s;
+	return (s);
 }
 
