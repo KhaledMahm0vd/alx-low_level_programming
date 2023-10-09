8332e96622908320e99f2918e8220f9f165ead83
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-10-09 23:09:36 +0300

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 #include <stdlib.h>
 /**
@@ -9,33 +10,33 @@
   */
 char *argstostr(int ac, char **av)
 {
-  char *arg = NULL;
-  int i, j, k;
-  size_t size = 0;
+	char *arg = NULL;
+	int i, j, k;
+	size_t size = 0;
 
-  for (i = 0; i < ac; i++)
-  {
-    size += strlen(av[i]) + 1;
-  }
+	for (i = 0; i < ac; i++)
+	{
+	size += strlen(av[i]) + 1;
+	}
 
-  arg = malloc(size);
-  if (arg == NULL)
-    return NULL;
+	arg = malloc(size);
+	if (arg == NULL)
+		return (NULL);
 
-  k = 0;
-  for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j]; j++)
-    {
-      arg[k] = av[i][j];
-      k++;
-    }
-    arg[k] = '\n';
-    k++;
-  }
+	k = 0;
+	for (i = 0; i < ac; i++)
+		{
+		for (j = 0; av[i][j]; j++)
+			{
+			arg[k] = av[i][j];
+			k++;
+		}
+	arg[k] = '\n';
+	k++;
+	}
 
-  arg[k] = '\0';
+	arg[k] = '\0';
 
-  return arg;
+	return (arg);
 }
 
