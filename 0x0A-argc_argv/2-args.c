a87d601106e03f2f5169b4d095bbc637b3430306
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-10-10 09:26:43 +0300

    22

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,13 @@
  *
  * Return: Always 0 (Success)
  */
- 
 int main(int argc, char *argv[])
 {
-  for (int i = 1; i < argc; i++)
-  {
-    printf("%s\n", argv[i]);
-  }
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
-  return 0;
+	return (0);
 }
